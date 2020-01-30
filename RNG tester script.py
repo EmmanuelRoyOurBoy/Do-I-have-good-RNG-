271613f36284840676f9a91a3de89dd467729231
Emmanuel Roy <54725843+EmmanuelRoyOurBoy@users.noreply.github.com>
2020-01-30 11:11:52 -0600

    Update RNG tester script.py

--- RNG tester script.py
+++ RNG tester script.py
@@ -1,2 +1,5 @@
 import random
 PlayerName=input("What is your name? ")
+OppositionList = random.shuffle["Bob The Builder","Shrek","Waluigi","Emmauel","Another Fire Emblem character","Chris Hansen","R Kelly","Bill Cosby","Anon from 4chan","Naruto","Goku","Reggie Fils-Aimé","Sakurai","Sanic","Brazzers","Etika","Hentai Protagonist"]
+Opponent = OppositionList[0]
+print("Today you will be facing"+" "+Opponent)
