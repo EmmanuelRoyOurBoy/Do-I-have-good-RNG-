028dced91a127d077eb2e4265781e84f02e0ae66
Emmanuel Roy <emmanuelroy@emmanuelroy.tech>
2020-02-25 11:16:50 -0600

    Update RNG tester script.py

--- RNG tester script.py
+++ RNG tester script.py
@@ -1,11 +1,16 @@
+#This game was made by Emmanuel Roy as a 5 minute coding challenge. The code for this is horrendously bad, so please excuse any errors.
+#The game is also fundementally broken when displaying results but that adds onto the RNG element of this game.
 import random
-PlayerName=str(input("What is your name? "))
-OppositionList =["Bob The Builder","Shrek","Waluigi","Emmauel","Another Fire Emblem character","Chris Hansen","R Kelly","Bill Cosby","Anon from 4chan","Naruto","Goku","Reggie Fils-Aimé","Sakurai","Sanic","Brazzers","Etika","Hentai Protagonist"]
-Opponent =random.choice(OppositionList)
-print("Today you will be facing "+Opponent)
-QuestionFight=input("Are you READY TO RUMBLE!!!!!!(YES OR YES)")
-areyouready = False
-if QuestionFight == "YES"or "yes" or "Yes" or "yEs" or  "yeS" or "YeS" or "yES" or "YEs":
-    areyouready=True
-if areyouready == True:
-    print("Lets begin then")
+Challengers = ["Bob The Builder","Shrek","Waluigi","Another Fire Emblem character","Chris Hansen","Bill Cosby","Anon from 4chan","Reggie Fils-Aimé","Sanic","Brazzers","Hentai Protagonist"]
+Opponent =random.choice(Challengers)
+P1Name=str(input("What is your name? "))
+print(str.format("{} will be facing {}!",P1Name,Opponent))
+RandomNumber = int(random.randint(1,101))
+P1ChosenNumber = int(input("Pick a number from 1-100! "))
+OpponentChosenNumber = int(random.randint(1,101))
+print(str.format("{} chose {} and {} chose {}! ", P1Name, P1ChosenNumber, Opponent, OpponentChosenNumber))
+print(str.format("The number was {}", RandomNumber))
+if (P1ChosenNumber - RandomNumber) >= (OpponentChosenNumber - RandomNumber) or (RandomNumber - P1ChosenNumber) <= (RandomNumber - OpponentChosenNumber):
+    print(str.format("{} wins!",P1Name))
+else:
+    print(str.format("{} wins!",Opponent))
